b868012929eabfb98b8b2aba6dcc305b41f2fe83
Marco Antonio <marco.perez.cetis76@gmail.com>
2020-05-13 15:50:03 -0500

    casi lista flatan los periodos

--- js/busca.js
+++ js/busca.js
@@ -1,5 +1,8 @@
 var obj;
-var salida = '';
+var salidaA = '';
+var salidaB = '';
+var salidaC = '';
+var salidaD = '';
 var valor;
 
 var appi = "https://script.google.com/macros/s/AKfycbzloNhYKVTnxaE8XRAoHceu6bDqug-4_qPck6wHR5nC0hMZ4ac/exec";
@@ -28,39 +31,168 @@ function ft(appi) {
 
 function pintar() {
     var lista = JSON.parse(localStorage.getItem("obj"));
+   
+    var newArrayA = lista.filter(function (el) {
+        return (el.GRUPO === "A");
+    });
+   /*  console.log(newArrayA); */
     /* console.log(lista); */
-    lista.forEach(e => {
+    newArrayA.forEach(e => {
 
 
-        salida = salida + `
+        salidaA = salidaA + `
        <div class="col s12 m6 l6">
               <div class="card sticky-action black">
                 <div class="card-content">
                   <span class="card-title activator white-text">${e.COMPLETO}</span>
-                  <span style="border-radius: 10px;font-size: 1em;font-weight: bold;" class="new badge grey" data-badge-caption="${e.ASISTENCIAS}"></span>
+                  <span id="${e.ID}-asi" style="border-radius: 10px;font-size: 1em;font-weight: bold;" class="new badge" data-badge-caption="${e.ASISTENCIAS}"></span>
                   <!--<span style="border-radius: 10px;font-size: 0.8em;" class="new badge blue left" data-badge-caption="${e.NICK}"></span> -->
-                  <span style="border-radius: 10px;font-size: 0.8em;" class="new badge red left" data-badge-caption="${e.EQUIPO}"></span>
-                  <span style="border-radius: 10px;font-size: 0.8em;" class="new badge orange left" data-badge-caption="${e.ID}"></span>
+                  <span style="border-radius: 10px;font-size: 1em;" class="new badge datos left" data-badge-caption="${e.EQUIPO}"></span>
+                  <span style="border-radius: 10px;font-size: 1em;" class="new badge datos left" data-badge-caption="${e.ID}"></span>
                   <br>
-                  <a onclick="envia('${e.COMPLETO}','${e.NICK}','${e.EQUIPO}','${e.GRUPO}','${e.ID}');" class="btn-floating halfway-fab waves-effect waves-light green"><i class="material-icons">add</i></a>
+                  <a id="${e.ID}-btn"  
+                     onclick="envia('${e.COMPLETO}','${e.NICK}','${e.EQUIPO}','${e.GRUPO}','${e.ID}','${e.ASISTENCIAS}');" 
+                     class="btn-floating halfway-fab waves-effect waves-black light-green">
+                    <i id="${e.ID}-icon" 
+                        class="material-icons" >
+                        add
+                    </i>
+                </a>
                 </div>
-                <div class="card-reveal">
+               <!-- <div class="card-reveal">
                   <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
     
-                </div>
+                </div> -->
               </div>
             </div>
        `;
     });
 
-    document.getElementById("lista").innerHTML = salida;
+    document.getElementById("test1").innerHTML = salidaA;
+
+    var newArrayB = lista.filter(function (el) {
+        return (el.GRUPO === "B");
+    });
+    /* console.log(newArray); */
+    /* console.log(lista); */
+    newArrayB.forEach(e => {
+
+
+        salidaB = salidaB + `
+       <div class="col s12 m6 l6">
+              <div class="card sticky-action black">
+                <div class="card-content">
+                  <span class="card-title activator white-text">${e.COMPLETO}</span>
+                  <span id="${e.ID}-asi" style="border-radius: 10px;font-size: 1em;font-weight: bold;" class="new badge" data-badge-caption="${e.ASISTENCIAS}"></span>
+                  <!--<span style="border-radius: 10px;font-size: 0.8em;" class="new badge blue left" data-badge-caption="${e.NICK}"></span> -->
+                  <span style="border-radius: 10px;font-size: 1em;" class="new badge datos left" data-badge-caption="${e.EQUIPO}"></span>
+                  <span style="border-radius: 10px;font-size: 1em;" class="new badge datos left" data-badge-caption="${e.ID}"></span>
+                  <br>
+                  <a id="${e.ID}-btn"  
+                     onclick="envia('${e.COMPLETO}','${e.NICK}','${e.EQUIPO}','${e.GRUPO}','${e.ID}','${e.ASISTENCIAS}');" 
+                     class="btn-floating halfway-fab waves-effect waves-black light-green">
+                    <i id="${e.ID}-icon" 
+                        class="material-icons" >
+                        add
+                    </i>
+                </a>
+                </div>
+               <!-- <div class="card-reveal">
+                  <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+    
+                </div> -->
+              </div>
+            </div>
+       `;
+    });
+
+    document.getElementById("test2").innerHTML = salidaB;
+
+
+    var newArrayC = lista.filter(function (el) {
+        return (el.GRUPO === "C");
+    });
+    /* console.log(newArray); */
+    /* console.log(lista); */
+    newArrayC.forEach(e => {
+
+
+        salidaC = salidaC + `
+       <div class="col s12 m6 l6">
+              <div class="card sticky-action black">
+                <div class="card-content">
+                  <span class="card-title activator white-text">${e.COMPLETO}</span>
+                  <span id="${e.ID}-asi" style="border-radius: 10px;font-size: 1em;font-weight: bold;" class="new badge" data-badge-caption="${e.ASISTENCIAS}"></span>
+                  <!--<span style="border-radius: 10px;font-size: 0.8em;" class="new badge blue left" data-badge-caption="${e.NICK}"></span> -->
+                  <span style="border-radius: 10px;font-size: 1em;" class="new badge datos left" data-badge-caption="${e.EQUIPO}"></span>
+                  <span style="border-radius: 10px;font-size: 1em;" class="new badge datos left" data-badge-caption="${e.ID}"></span>
+                  <br>
+                  <a id="${e.ID}-btn"  
+                     onclick="envia('${e.COMPLETO}','${e.NICK}','${e.EQUIPO}','${e.GRUPO}','${e.ID}','${e.ASISTENCIAS}');" 
+                     class="btn-floating halfway-fab waves-effect waves-black light-green">
+                    <i id="${e.ID}-icon" 
+                        class="material-icons" >
+                        add
+                    </i>
+                </a>
+                </div>
+               <!-- <div class="card-reveal">
+                  <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+    
+                </div> -->
+              </div>
+            </div>
+       `;
+    });
+
+    document.getElementById("test3").innerHTML = salidaC;
+
+
+    var newArrayD = lista.filter(function (el) {
+        return (el.GRUPO === "D");
+    });
+    /* console.log(newArray); */
+    /* console.log(lista); */
+    newArrayD.forEach(e => {
+
+
+        salidaD = salidaD + `
+       <div class="col s12 m6 l6">
+              <div class="card sticky-action black">
+                <div class="card-content">
+                  <span class="card-title activator white-text">${e.COMPLETO}</span>
+                  <span id="${e.ID}-asi" style="border-radius: 10px;font-size: 1em;font-weight: bold;" class="new badge" data-badge-caption="${e.ASISTENCIAS}"></span>
+                  <!--<span style="border-radius: 10px;font-size: 0.8em;" class="new badge blue left" data-badge-caption="${e.NICK}"></span> -->
+                  <span style="border-radius: 10px;font-size: 1em;" class="new badge datos left" data-badge-caption="${e.EQUIPO}"></span>
+                  <span style="border-radius: 10px;font-size: 1em;" class="new badge datos left" data-badge-caption="${e.ID}"></span>
+                  <br>
+                  <a id="${e.ID}-btn"  
+                     onclick="envia('${e.COMPLETO}','${e.NICK}','${e.EQUIPO}','${e.GRUPO}','${e.ID}','${e.ASISTENCIAS}');" 
+                     class="btn-floating halfway-fab waves-effect waves-black light-green">
+                    <i id="${e.ID}-icon" 
+                        class="material-icons" >
+                        add
+                    </i>
+                </a>
+                </div>
+               <!-- <div class="card-reveal">
+                  <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+    
+                </div> -->
+              </div>
+            </div>
+       `;
+    });
+
+    document.getElementById("test4").innerHTML = salidaD;
+
     console.log("Se pinto la info");
 }
 
 
 
-function envia(c, n, e, g, i) {
-    
+function envia(c, n, e, g, i, a) {
+
     var fecha = new Date();
     var hoy = JSON.stringify(fecha);
     var hora = getLogoffTime();
@@ -83,13 +215,16 @@ function envia(c, n, e, g, i) {
 
     iframe.src = envioUrl;
     document.getElementById("contenido").appendChild(iframe);
-    setTimeout('limpiarContenido()', 3000);   
-   
+    setTimeout('limpiarContenido()', 3000);
+    document.getElementById(i + "-btn").className += " usado";
+    document.getElementById(i + "-icon").innerHTML = "check";
+    document.getElementById(i + "-icon").style.color = "#18f018";
+    document.getElementById(i + "-asi").setAttribute('data-badge-caption', parseInt(a) + 1);;
 }
 
 var limpiarContenido = function () {
     document.getElementById("contenido").style.display = "none";
-    
+
 }
 
 function getLogoffTime() {
